3a48f4cfdba7c44943b69d2318e2a99f55ccf8c2
alansmello <alansmello@gmail.com>
2021-12-30 11:20:28 -0300

    incrementado desabilitar button quando count chegar a -10 ou 10, e tambem o numero ficar vermelho quando negativo

--- JavaScript/assets/js/scripts.js
+++ JavaScript/assets/js/scripts.js
@@ -1,12 +1,41 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
+
 var currentNumber = 0;
+
+var subtract = document.getElementById('subtract');
+
+var add = document.getElementById('add');
+
+subtract.addEventListener("click",decrement);
+add.addEventListener("click", increment);
+
+
+
 function increment(){
     currentNumber = currentNumber +1;
     currentNumberWrapper.innerHTML = currentNumber;
-
+   
+    if(currentNumber >= 0){
+        currentNumberWrapper.style.color = "black";
+    }if(currentNumber >=10){
+        add.disabled = true;
+        add.style.background = "gray";
+        add.innerText = ''
+    }
 }
+
+
+
 function decrement(){
     currentNumber = currentNumber -1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = "red";
+    }if(currentNumber <=-10){
+        subtract.disabled = true;
+        subtract.style.background = "gray"
+        subtract.innerText = '';
+    
+    }
 
 }
